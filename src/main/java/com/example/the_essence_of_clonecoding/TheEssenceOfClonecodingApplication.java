22c1c2fd34775661578415c8420cda00297e2562
LeeSooo <dltn990930@naver.com>
2023-04-24 00:51:13 +0900

    feat : #1 프로젝트 생성 (2023-04-24)
     Spring으로 URL 단축 서비스 서버 구축하기 위해 프로젝트를 생성하여 올림.

---
+++ src/main/java/com/example/the_essence_of_clonecoding/TheEssenceOfClonecodingApplication.java
@@ -0,0 +1,14 @@
+package com.example.the_essence_of_clonecoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheEssenceOfClonecodingApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		SpringApplication.run(TheEssenceOfClonecodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/the_essence_of_clonecoding/TheEssenceOfClonecodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.the_essence_of_clonecoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheEssenceOfClonecodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
